eac2fc7b632ac2079788c356efe7d630dac72c26
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-19 01:40:42 +0300

    changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Book
 # function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/templates/list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 # Class-based view
@@ -14,5 +14,5 @@ from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/templates/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
