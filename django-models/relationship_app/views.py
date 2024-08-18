75497fe57644237096a883f1ab9ddd44d884a759
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-19 02:12:54 +0300

    Add changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -16,3 +16,24 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# SetUp Authentication Views
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+class UserLoginView(LoginView):
+    template_name = 'login.html'
+
+class UserLogoutView(LogoutView):
+    template_name = 'logout.html'
+
+class UserRegistrationView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        return super().form_valid(form)
