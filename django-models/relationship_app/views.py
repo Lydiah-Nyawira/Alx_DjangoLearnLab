3cff7a51bc3509bdf3f6e487864767d201ab24e1
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-18 23:24:42 +0300

    Minor changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Book
-from django.views.generic.detail import DetailView
-from .models import Library
-
 
 # Create your views here.
+# function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    response_text = "List of Books:\n\n"
+    for book in books:
+        response_text += f"{book.title} by {book.author.name}\n"
+    return HttpResponse(response_text, content_type='text/plain')
+
+
+# Class-based view
+from django.views.generic.detail import DetailView
+from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
