f899d62a1cd1c61bbbfdd3da300e1f7b9fade63d
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-18 19:02:48 +0300

    added relationship_app

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,41 @@
+import django
+import os
+
+# Setup Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+def query_all_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = author.books.all()
+        for book in books:
+            print(f"Book: {book.title}")
+    except Author.DoesNotExist:
+        print("Author not found.")
+
+def list_all_books_in_library(library_name):
+    try:
+        library = library.objects.get(name=library_name)
+        books = library.books.all()
+        for book in books:
+            print(f"Book: {book.title}")
+    except Library.DoesNotExist:
+        print("Library not found.")
+
+def retrieve_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian()
+        print(f"Librarian: {librarian.name}")
+    except Library.DoesNotExist:
+        print("Library not found.")
+    except Librarian.DoesNotExist:
+        print("Librarian not assigned.")
+
+if __name__ == "__main__":
+    query_all_books_by_author() 
+    list_all_books_in_library()
+    retrieve_librarian_for_library()           
