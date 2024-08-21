4e49be610afb8e73f9c70c790a3bd7472e480934
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-21 09:10:00 +0300

    Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,41 +1,28 @@
-import django
 import os
+import django
 
-# Setup Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+# SetUp Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-def query_all_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        books = Book.objects.filter(author=author)
-        for book in books:
-            print(f"Book: {book.title}")
-    except Author.DoesNotExist:
-        print("Author not found.")
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    for book in books:
+        print(book.title)
 
-def list_all_books_in_library(library_name):
-    try:
-        library = library.objects.get(name=library_name)
-        books = library.books.all()
-        for book in books:
-            print(f"Book: {book.title}")
-    except Library.DoesNotExist:
-        print("Library not found.")
+# List books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    for book in books:
+        print(book.title)
 
-def retrieve_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        librarian = Librarian.objects.get(library=library)
-        print(f"Librarian: {librarian.name}")
-    except Library.DoesNotExist:
-        print("Library not found.")
-    except Librarian.DoesNotExist:
-        print("Librarian not assigned.")
-
-if __name__ == "__main__":
-    query_all_books_by_author() 
-    list_all_books_in_library()
-    retrieve_librarian_for_library()           
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    print(librarian.name)                
