eb0ebef741e8e661a3cb6bbd49f77402a015a6dc
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-22 15:03:43 +0300

    Add changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -58,12 +58,12 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin.html')
+    return render(request, 'admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian.html')
+    return render(request, 'librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member.html')
+    return render(request, 'member_view.html')
