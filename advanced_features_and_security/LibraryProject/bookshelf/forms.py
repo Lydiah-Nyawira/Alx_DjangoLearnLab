1524855627852eda6086e4ca3bf43e5393f5c821
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-25 16:46:13 +0300

    Added changes

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,14 @@
 from django import forms
+from .models import Book
 
-class SearchForm(forms.Form):
-    search = forms.CharField(max_length=100)
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']  # List fields you want in the form
+
+    # Optional: Add custom validation or form logic here
+    def clean_publication_year(self):
+        publication_year = self.cleaned_data.get('publication_year')
+        if publication_year < 0:
+            raise forms.ValidationError('Publication year cannot be negative.')
+        return publication_year
