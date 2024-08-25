fcc221843150ddeaa8fbbd14c050c7c01764674b
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-08-25 16:54:24 +0300

    Added changes

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view_book', raise_exception=True)
 def view_book(request, book_id):
