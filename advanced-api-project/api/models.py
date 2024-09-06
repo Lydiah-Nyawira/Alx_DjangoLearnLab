dea42f34a48965588f4046c4421ad455e0134c0e
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-09-06 17:38:28 +0300

    Defined Data Models

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Model representing an author
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        self.name
+# Model representing a book
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.PositiveIntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    def __str__(self):
+        self.title        
