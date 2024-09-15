f1d5b7915a68eade49bcc5de657181516f9dad25
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-09-15 13:43:07 +0300

    Modified urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     # Blog comments urls
     path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
-    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit_comment'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
 
 ]
